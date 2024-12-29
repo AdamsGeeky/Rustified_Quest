dfd76ed0c11196a80dd80376d4628487c3b74262
AdamsGeeky <muhdadamu0008@gmail.com>
2024-12-29 15:12:44 +0100

    feat: varibale

---
+++ fundamentals/module-01/array.rs
@@ -0,0 +1,14 @@
+fn main(){
+    let zeros: [u8; 6] = [0, 0, 0, 0, 0, 0];
+    let zeros_short: [u8; 6] = [0, 6];
+
+    assert_eq!(zeros, zeros_short);
+
+    let arr =  [1,2,3,4,5,6];
+    let slice = &arr[2..5];
+    println!("arr = {:?}", arr);
+    println!("slice = {:?}", slice);
+
+    let xs = [2,4,6];
+    let mut_slice = &mut xs[0..2];
+}
